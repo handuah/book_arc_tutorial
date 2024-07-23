d4067f4d853aa234fa60fbf8d2c6582ef964bf7b
handuah <handuah@gmail.com>
2024-07-23 12:51:27 +0000

    setting up the screen with the colors

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -11,8 +11,169 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home,'),
+      body: SafeArea(
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                  borderSide: BorderSide.none,
+                ),
+                filled: true,
+                fillColor: Colors.grey.withOpacity(0.3),
+                prefixIcon: Icon(Icons.search_outlined),
+                suffixIcon: Icon(Icons.mic),
+                labelText: 'Search',
+              ),
+            ),
+
+            // Space
+            const SizedBox(
+              height: 20.0,
+            ),
+
+            // Container
+            Container(
+              height: 200.0,
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+            ),
+
+            // Space
+            const SizedBox(
+              height: 20.0,
+            ),
+
+            // Text
+            const Text(
+              'Stories',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 30.0,
+              ),
+            ),
+
+            // Space
+            const SizedBox(
+              height: 20.0,
+            ),
+
+            // Container
+            Container(
+              height: 200.0,
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: Colors.green,
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: ListView(
+                padding: EdgeInsets.all(8.0),
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Container(
+                    height: 100.0,
+                    width: 100.0,
+                    decoration: BoxDecoration(
+                      color: Colors.purple,
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                  ),
+                  // Space
+                  const SizedBox(
+                    width: 20.0,
+                  ),
+                  Container(
+                    height: 100.0,
+                    width: 100.0,
+                    decoration: BoxDecoration(
+                      color: Colors.purple,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  // Space
+                  const SizedBox(
+                    width: 20.0,
+                  ),
+                  Container(
+                    height: 100.0,
+                    width: 100.0,
+                    decoration: BoxDecoration(
+                      color: Colors.purple,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  // Space
+                  const SizedBox(
+                    width: 20.0,
+                  ),
+                  Container(
+                    height: 100.0,
+                    width: 100.0,
+                    decoration: BoxDecoration(
+                      color: Colors.purple,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            // Space
+            const SizedBox(
+              height: 20.0,
+            ),
+
+            // Text
+            const Text(
+              'New Arrivals',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 30.0,
+              ),
+            ),
+
+            // Space
+            const SizedBox(
+              height: 20.0,
+            ),
+
+            // Row
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  height: 200.0,
+                  width: 150.0,
+                  decoration: BoxDecoration(
+                    color: Colors.purple,
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                // Space
+                // const SizedBox(
+                //   width: 20.0,
+                // ),
+                Container(
+                  height: 200.0,
+                  width: 150.0,
+                  decoration: BoxDecoration(
+                    color: Colors.purple,
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+              ],
+            ),
+
+            // Space
+            const SizedBox(
+              height: 20.0,
+            ),
+          ],
+        ),
       ),
     );
   }
