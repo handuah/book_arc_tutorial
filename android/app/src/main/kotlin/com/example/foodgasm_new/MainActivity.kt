e03aae285854e62ed5d34b1998e10c82954f9d7a
handuah <handuah@gmail.com>
2024-07-23 12:27:49 +0000

    bottomnavprep

---
+++ android/app/src/main/kotlin/com/example/foodgasm_new/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.foodgasm_new
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:foodgasm_new/books.dart';
+import 'package:foodgasm_new/screens/cart.dart';
+import 'package:foodgasm_new/screens/dashboard.dart';
+import 'package:foodgasm_new/screens/favorites.dart';
+import 'package:foodgasm_new/screens/home.dart';
+import 'package:foodgasm_new/screens/user.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Navigation(),
+    );
+  }
+}
+
+class Navigation extends StatefulWidget {
+  const Navigation({super.key});
+
+  @override
+  State<Navigation> createState() => _NavigationState();
+}
+
+class _NavigationState extends State<Navigation> {
+  int currentIndex = 0;
+
+  List screens = [
+    Home(),
+    Books(),
+    Cart(),
+    Favourites(),
+    User(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        onTap: (int index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        selectedItemColor: Color(0xFFB94D59),
+        showSelectedLabels: true,
+        showUnselectedLabels: false,
+        currentIndex: currentIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bar_chart_outlined),
+            label: 'Charts',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_bag_outlined),
+            label: 'Shop',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bookmark_outline),
+            label: 'Bookmark',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outlined),
+            label: 'User',
+          ),
+        ],
+      ),
+      body: screens[currentIndex],
+    );
+  }
+}
