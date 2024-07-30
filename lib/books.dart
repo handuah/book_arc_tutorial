221b96549910f87107e8989eeeb5b8acedb1adc8
handuah <handuah@gmail.com>
2024-07-30 13:28:08 +0000

    created stacked starting page at charts bottomnavitem

--- lib/books.dart
+++ lib/books.dart
@@ -11,8 +11,42 @@ class _BooksState extends State<Books> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home,'),
+      body: Stack(
+        children: [
+          // First Child
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(
+                  'https://clubprana.com/wp-content/uploads/2020/10/Club-4th-min.jpg',
+                ),
+                fit: BoxFit.fitHeight,
+              ),
+            ),
+          ),
+
+          Container(
+            color: Colors.pink.withOpacity(0.5),
+          ),
+
+          const Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Icon(
+                  Icons.facebook_outlined,
+                  size: 48.0,
+                  color: Colors.white,
+                ),
+                Text(
+                  'Facebook',
+                  style: TextStyle(color: Colors.white, fontSize: 28.0),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
